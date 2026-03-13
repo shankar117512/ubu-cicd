ae8577d81b1a0370ac0d9a9317e2b6c076826b75
shankar117512 <shankar117512@gmail.com>
2026-03-13 09:22:42 +0000

    entrypoint

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+echo "Running migrations..."
 python manage.py migrate --noinput
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Starting Gunicorn..."
 gunicorn myproject.wsgi:application --bind 0.0.0.0:8000
