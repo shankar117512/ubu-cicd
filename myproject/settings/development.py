d9b337f3b3cdd221f36508e9fc5e9df231ea6965
shankar117512 <shankar117512@gmail.com>
2026-03-13 07:48:51 +0000

    devlopment.py

--- myproject/settings/development.py
+++ myproject/settings/development.py
@@ -6,3 +6,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CORS_ALLOW_ALL_ORIGINS = True
 INSTALLED_APPS += ['debug_toolbar']
 MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
+CSRF_TRUSTED_ORIGINS = ["https://ubu-cicd-development.up.railway.app"]
+ALLOWED_HOSTS = ['ubu-cicd-development.up.railway.app']
