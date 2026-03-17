61a1b043948492a667a6efe2c3517a15b3a5b308
shankar117512 <shankar117512@gmail.com>
2026-03-17 15:31:23 +0000

    staging.py

--- myproject/settings/staging.py
+++ myproject/settings/staging.py
@@ -3,3 +3,5 @@ from .base import *
 DEBUG = False
 ENVIRONMENT = 'staging'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ALLOWED_HOSTS = ['creative-flow-staging.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://creative-flow-staging.up.railway.app']
